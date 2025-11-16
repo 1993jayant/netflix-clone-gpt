4debbd82f9cde7a8d7a1f518031db1c7043372fb
Jayant <1993jayant@gmail.com>
2025-11-16 15:39:18 +1300

    [ADD] Tailwind Css

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl font-bold text-red-400'>
+        hi there, how are you?
+      </div>
+        
+    </>
+  )
+}
+
+export default App
